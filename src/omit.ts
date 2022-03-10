4436ff745aa43e204b10584cb25788ad6b6562ac
PARKER\Parker <parkers@mail.ru>
2022-03-10 15:19:35 +0300

    Add task omit

--- src/omit.ts
+++ src/omit.ts
@@ -1,10 +1,10 @@
-type FIXME = any;
+type FIXME<T extends Record<any, any>, K extends keyof T> = Omit<T, K>;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const omit = <T extends Record<any, any>, K extends keyof T>(
   obj: T,
   keyToOmit: K
-): FIXME => {
+): FIXME<T, K> => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { [keyToOmit]: _, ...withoutKey } = obj;
   return withoutKey;
